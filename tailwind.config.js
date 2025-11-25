10004767b00a63ac339bcba74350f71939adaa25
Animalgeek <animalgeek.dev@gmail.com>
2025-11-25 14:08:57 +0100

    add: tailwind base settings

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './resources/**/*.{vue,js,ts,jsx,tsx,html}',
+    './src/**/*.{js,php,vue,html}',
+    './tests/**/*.{js,php,vue,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+};
